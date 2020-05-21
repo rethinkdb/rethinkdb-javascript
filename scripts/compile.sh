074df334655762401bf026df9dffdf751b01f83d
Gabor Boros <gabor.brs@gmail.com>
2020-05-21 08:24:21 +0200

    Adjust packaging

--- scripts/compile.sh
+++ scripts/compile.sh
@@ -4,3 +4,6 @@ ls -1 src/*.coffee | while read fname; do
   fname=$(basename "${fname}" | cut -f 1 -d '.')
   coffee -b -p -c "src/${fname}.coffee" > "dist/${fname}.js"
 done
+
+cp package*.json dist/
+cp README.md dist/
